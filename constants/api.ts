b5ad413ad4161edf57ca75954b8e70356d1b9cb5
Yash Chauhan <yc9274@gmail.com>
2021-08-12 14:31:13 +0530

    added: nProgress

---
+++ constants/api.ts
@@ -0,0 +1,5 @@
+export const Api = {
+  base: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+  api: `${process.env.NEXT_PUBLIC_API_URL}`,
+  storage: `${process.env.NEXT_PUBLIC_STORAGE_URL}/`,
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/dist/client/router';
-import strings from '../lang/Lang';
+import type { AppProps } from 'next/app';
+import Router, { useRouter } from 'next/router';
+import NProgress from 'nprogress';
 import { useEffect } from 'react';
+import strings from '../lang/Lang';
+import '../styles/globals.css';
+
+Router.events.on('routeChangeStart', () => { NProgress.start() })
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
