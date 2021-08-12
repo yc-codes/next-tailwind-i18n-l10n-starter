8faa0ef2528cfb4a79d98574b1248458ee9b523b
Yash Chauhan <yc9274@gmail.com>
2021-08-12 12:58:21 +0530

    added: i18n + l10n

---
+++ lang/strings/en.tsx
@@ -0,0 +1,13 @@
+const validations = {
+  enter_phone_number: "Enter Phone Number",
+  enter_valid_phone_number: "Enter valid phone number",
+}
+
+const data = {
+  ...validations,
+  text_direction: "ltr",
+  locale: 'en',
+  app_name: "Next.js Base Project ",
+}
+
+export default data
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/dist/client/router';
+import strings from '../lang/Lang';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    strings.setLanguage(router?.locale ?? 'en');
+  }, [router.locale])
+
   return <Component {...pageProps} />
 }
 export default MyApp
